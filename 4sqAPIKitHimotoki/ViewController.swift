9bb6f1713078a2ab9a82ac135d2122e673ae3014
koogawa <koogawa.app@gmail.com>
2016-05-29 13:36:59 +0900

    Implement ViewController

--- 4sqAPIKitHimotoki/ViewController.swift
+++ 4sqAPIKitHimotoki/ViewController.swift
@@ -3,16 +3,37 @@
 //  4sqAPIKitHimotoki
 //
 //  Created by koogawa on 2016/05/29.
-//  Copyright © 2016年 koogawa. All rights reserved.
+//  Copyright © 2016 koogawa. All rights reserved.
 //
 
 import UIKit
+import APIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    var venues = [Venue]() {
+        didSet {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.tableView.reloadData()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let request = GetVenueList()
+
+        Session.sendRequest(request) { response in
+            switch response {
+
+            case .Success(let responses):
+                self.venues = responses.response.venues
+
+            case .Failure(let error):
+                print(error)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UITableViewDataSource
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.venues.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let venue = venues[indexPath.row]
+        cell.textLabel?.text = venue.name
+
+        return cell
+    }
 }
 
