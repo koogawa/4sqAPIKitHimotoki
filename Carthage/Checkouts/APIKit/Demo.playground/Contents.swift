004ef5774095ef6c6f24d5e659c4981452a3ea13
koogawa <koogawa.app@gmail.com>
2016-05-29 13:39:05 +0900

    Add Carthage files

---
+++ Carthage/Checkouts/APIKit/Demo.playground/Contents.swift
@@ -0,0 +1,72 @@
+import XCPlayground
+import UIKit
+import APIKit
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+//: Step 1: Define request protocol
+protocol GitHubRequestType: RequestType {
+
+}
+
+extension GitHubRequestType {
+    var baseURL: NSURL {
+        return NSURL(string: "https://api.github.com")!
+    }
+}
+
+//: Step 2: Create model object
+struct RateLimit {
+    let count: Int
+    let resetDate: NSDate
+
+    init?(dictionary: [String: AnyObject]) {
+        guard let count = dictionary["rate"]?["limit"] as? Int else {
+            return nil
+        }
+
+        guard let resetDateString = dictionary["rate"]?["reset"] as? NSTimeInterval else {
+            return nil
+        }
+
+        self.count = count
+        self.resetDate = NSDate(timeIntervalSince1970: resetDateString)
+    }
+}
+
+//: Step 3: Define request type conforming to created request protocol
+// https://developer.github.com/v3/rate_limit/
+struct GetRateLimitRequest: GitHubRequestType {
+    typealias Response = RateLimit
+
+    var method: HTTPMethod {
+        return .GET
+    }
+
+    var path: String {
+        return "/rate_limit"
+    }
+
+    func responseFromObject(object: AnyObject, URLResponse: NSHTTPURLResponse) throws -> Response {
+        guard let dictionary = object as? [String: AnyObject],
+              let rateLimit = RateLimit(dictionary: dictionary) else {
+            throw ResponseError.UnexpectedObject(object)
+        }
+
+        return rateLimit
+    }
+}
+
+//: Step 4: Send request
+let request = GetRateLimitRequest()
+
+Session.sendRequest(request) { result in
+    switch result {
+    case .Success(let rateLimit):
+        print("count: \(rateLimit.count)")
+        print("reset: \(rateLimit.resetDate)")
+
+    case .Failure(let error):
+        print("error: \(error)")
+    }
+}
